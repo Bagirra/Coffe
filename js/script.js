fe16f7717666e8f3878b89eb7bb55f1b4586382f
Bagirra <99732805+Bagirra@users.noreply.github.com>
2022-12-19 19:23:27 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function functie() {
+    document.getElementById('rez').style.fontSize="40px"
+
+}
+function functie2() {
+    document.getElementById("et1").innerHTML="";
+}
+function functie3() {
+    document.getElementById("et1").innerHTML="я поняла";
+}    
+var nume="Ivanov";
+        var prenume="Ivan";
+        var varsta="40";
+        document.getElementById('et5').innerHTML=nume;
+        document.getElementById('et6').innerHTML=prenume;
+        document.getElementById('et7').innerHTML=varsta;
+
+        console.log(30+4);
+        prompt('30+4')
+        alert("Внимание")       
